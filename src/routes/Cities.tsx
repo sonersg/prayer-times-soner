2dc49fa1d2317af944fb59b0d2a2a31721b6c235
Soner <sonersg@outlook.com>
2025-06-05 21:30:17 +0300

    2025June5

--- src/routes/Cities.tsx
+++ src/routes/Cities.tsx
@@ -1,9 +1,36 @@
 import { Link } from 'react-router';
+import { iller } from '../assets/iller';
 
 function Cities() {
+  function handleClick(city: string, lat: number | null, lon: number | null) {
+    console.log(city, lat, lon);
+    alert(city);
+  }
+
   return (
     <div>
-      Cities
+      <h1>Cities</h1>
+      <div
+        className='scroll-view'
+        style={{
+          overflow: 'auto',
+          maxHeight: '88vh',
+          width: '66%',
+          margin: '0 auto',
+        }}
+      >
+        {iller.map((il, index) => (
+          <div
+            key={index}
+            className='cities-item'
+            onClick={() => handleClick(il.name, il.latitude, il.longitude)}
+          >
+            <h3>
+              {il.name} - {il.code}
+            </h3>
+          </div>
+        ))}
+      </div>
       <Link to='/'>Go Home</Link>
     </div>
   );
